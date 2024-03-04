fa6d8467be66dff7a4cfca4794f91be5a1c8a149
Esmat Najafi <145345735+Esmatnajafi@users.noreply.github.com>
2024-03-04 15:17:14 +0100

    Add hexColor input for users

--- ChangeText/src/ColorBox.tsx
+++ ChangeText/src/ColorBox.tsx
@@ -6,7 +6,11 @@ const ColorBox = () => {
   const [buttonColor, setButtonColor] = useState("");
 
   const handleColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setColor(event.target.value);
+    const hexColor= event.target.value;
+    // hexcolor code to for example #000000
+    if(/^#[0-9A-Fa-f]{0,6}$/i.test(hexColor)){
+        setColor(hexColor);
+    }
   };
 
   const handleButtonClick = () => {
@@ -19,9 +23,9 @@ const ColorBox = () => {
       <div className="color-box" >
         <input
           type="text"
-          value={color}
+          value={color.slice(1)}    //remove the # symbol
           onChange={handleColorChange}
-          placeholder="Enter a color"
+          placeholder="skriva hexadecimala värden börja med #"
         />
       </div>
       <button onClick={handleButtonClick} style={{background: buttonColor}}>Change Color</button>
